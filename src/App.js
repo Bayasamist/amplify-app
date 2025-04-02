014c114c4568f2e36e5dd959612ba07a1acff4e1
Bayasa <bayasa.zed@gmail.com>
2025-04-02 12:09:53 +1000

    Auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, Button, Heading, View, Text } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        “Learn react” to “Welcome to Amplify!”
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <Heading level={1}>Welcome to Amplify!</Heading>
+        
+        <View className="auth-section">
+          <Heading level={2}>Authentication Demo</Heading>
+          <Text>Hello {user.username}</Text>
+          <Button onClick={signOut}>Sign Out</Button>
+        </View>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator (App);
